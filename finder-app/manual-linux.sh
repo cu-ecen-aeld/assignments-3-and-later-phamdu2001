b957ae65339c249495b6e88e859e30e396d8bed4
phamdu2001 <phamdu2001@gmail.com>
2025-04-17 14:09:32 +0700

    Part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "Starting kernel build"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j4 all
+    # make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    cp arch/${ARCH}/boot/Image ${OUTDIR}/
+    echo "Kernel build complete"
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +57,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo "Creating necessary directories"
+sudo mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+sudo mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr/bin usr/lib usr/sbin var/log
+echo "Base directories created successfully"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +70,69 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    echo "Configuring BusyBox"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean  # Clean any previous configuration
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig  # Use the default configuration
+    echo "BusyBox configuration complete"
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+echo "Building and installing BusyBox"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j$(nproc)
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+echo "BusyBox build and installation complete"
+
+if [ ! -f ${OUTDIR}/rootfs/bin/busybox ]; then
+    echo "Error: BusyBox binary not found in ${OUTDIR}/rootfs/bin/"
+    exit 1
+fi
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# filepath: /home/du/assignment-1-phamdu2001/finder-app/manual-linux.sh
+echo "Adding library dependencies to rootfs"
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+echo "Library dependencies added successfully"
 
 # TODO: Make device nodes
+echo "Creating device nodes"
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
+echo "Device nodes created successfully"
 
 # TODO: Clean and build the writer utility
+echo "Cleaning and building the writer utility"
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+cp writer ${OUTDIR}/rootfs/home/
+echo "Writer utility built and copied successfully"
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "Copying finder scripts and executables to rootfs"
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home/
+echo "Finder scripts and executables copied successfully"
 
 # TODO: Chown the root directory
+echo "Changing ownership of rootfs to root"
+sudo chown -R root:root ${OUTDIR}/rootfs
+echo "Ownership changed successfully"
 
 # TODO: Create initramfs.cpio.gz
+echo "Creating initramfs.cpio.gz"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+gzip -f ../initramfs.cpio
+echo "initramfs.cpio.gz created successfully"
